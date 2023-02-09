c03010e602960eb5eefe6607f45e9973c94b0484
Boskeroni <102393898+Boskeroni@users.noreply.github.com>
2023-02-09 12:06:08 +0000

    added some comments to explain stuff

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use hashbrown::HashMap;
 use std::io::{BufRead, BufReader, stdin, stdout, Write};
 use std::fs::File;
 
+// contains each letter found in the word and how many times it shows up
 type LetterTally = HashMap<char, i8>;
 
 fn convert_to_hash(val: &str) -> LetterTally {
     let mut letters_hash = LetterTally::new();
     for c in val.chars().into_iter() {
+        // if it isnt found adds tally of one
         *letters_hash.entry(c).or_insert(0) += 1;
     }
     letters_hash
@@ -15,6 +17,7 @@ fn convert_to_hash(val: &str) -> LetterTally {
 fn validate_word(master_hash: &LetterTally, check_word: &str) -> bool {
     let mut check_hash = LetterTally::new();
     for c in check_word.chars().into_iter() {
+        // if the word contains a letter
         if !master_hash.contains_key(&c) {
             return false
         }
@@ -33,7 +36,8 @@ fn main() {
     let mut scramble = String::new();
     stdin().read_line(&mut scramble).unwrap();
     scramble = scramble.trim().to_string();
-
+    
+    // just to disregard all the words longer than the scramble
     let scramble_len = scramble.len();
     let input_hash = convert_to_hash(&scramble);
     let file = BufReader::new(File::open("words.txt").unwrap());
@@ -43,18 +47,25 @@ fn main() {
     for line in file.lines() {
         let trial_word = line.unwrap();
         let word_len = trial_word.len();
+        
+        // dont bother checking words shorter than the longest or longer than the scramble
         if scramble_len < word_len || word_len < len_longest {
             continue;
         }
         if !validate_word(&input_hash, &trial_word) {
             continue;
         }
+        
+        // update the new longest word
+        // if it equals the scramble then we have a max length
         longest = trial_word;
         len_longest = longest.len();
         if len_longest == scramble_len {
             break;
         }
     }
+    
+    //output
     println!("the longest word is: {}", longest);
     stdin().read_line(&mut String::new()).unwrap();
 }
